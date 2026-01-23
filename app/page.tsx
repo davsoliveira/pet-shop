e8fbaab0d94cf15c68801344cb8be569aa88deb1
Davi Oliveira <davi.ol.santos@gmail.com>
2026-01-22 21:58:54 -0300

    feat: adicionar revalidatePath

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import { AppointmentForm } from '@/components/appointment-form/appointment-form';
 import { PeriodSection } from '@/components/period-section';
-import { APPOINTMENT_DATA, groupAppointmentByPeriod } from '@/utils';
 import { prisma } from '@/lib/prisma';
+import { groupAppointmentByPeriod } from '@/utils';
 
 export default async function Home() {
-  const appointment = await prisma.appointment.findMany();
-  console.log('AQUI', appointment);
+  const appointments = await prisma.appointment.findMany();
 
-  const periods = groupAppointmentByPeriod(APPOINTMENT_DATA);
+  const periods = groupAppointmentByPeriod(appointments);
 
   return (
     <div className="bg-background-primary p-6">
@@ -28,7 +27,7 @@ export default async function Home() {
         ))}
       </div>
 
-      <div className="fixed bottom-0 left-0 right-0 flex justify-center bg-[#23242C] py-[18px] px-6 md:bottom-6 md:right-6 md:left-auto md:top-auto md:w-auto md:bg-transparent md:p-0">
+      <div className="fixed bottom-0 left-0 right-0 flex justify-center bg-[#23242C] py-4.5 px-6 md:bottom-6 md:right-6 md:left-auto md:top-auto md:w-auto md:bg-transparent md:p-0">
         <AppointmentForm />
       </div>
     </div>
